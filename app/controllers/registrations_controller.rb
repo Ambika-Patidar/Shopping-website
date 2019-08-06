125dbab70428340c02723ca71a82ee2935a6557e
ambikapatidar <apatidar@gammastack.com>
2019-08-06 19:00:16 +0530

    Sign Up

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,23 @@
+class RegistrationsController < ApplicationController
+  def new 
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+       flash[:success] = "You Sign Up Successfully "
+       redirect_to  root_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+    def user_params
+      params.require(:registration).permit(:first_name, :last_name, :email, :password)
+    end
+end
