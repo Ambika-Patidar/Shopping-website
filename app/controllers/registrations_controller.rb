b7bd7f58292f1586f3ab04ac77601d6f64a0fc5e
ambikapatidar <apatidar@gammastack.com>
2019-08-07 19:37:00 +0530

    Indentation & add bootstrap classes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,9 +13,6 @@ class RegistrationsController < ApplicationController
     end
   end
 
-  def show
-  end
-
   private
     def user_params
       params.require(:registration).permit(:first_name, :last_name, :email, :password)
