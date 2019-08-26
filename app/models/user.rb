8796df8bf87b19862d3da476a43a2bd0413deb73
ambikapatidar <apatidar@gammastack.com>
2019-08-26 20:34:53 +0530

    updated sessions  controller and views and user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+  # Use ActiveModel has_secure_password
+
+  has_secure_password
+
   has_many :products
   has_one :cart
   has_many :orders
+  has_many :addresses
+  
   validates_presence_of :first_name, :last_name, :email
   validates :password ,length: { minimum: 6 }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "only allows valid emails" }, uniqueness: true
-  has_secure_password
   validates_format_of :first_name, :last_name, :with => /\A[a-zA-Z]+\z/
 end
