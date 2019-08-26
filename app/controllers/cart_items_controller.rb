342f2698caeda34b8d4570e79d9c48caabf9a469
ambikapatidar <apatidar@gammastack.com>
2019-08-26 20:00:16 +0530

    modified cart_items controller and cart_item views

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,64 +1,51 @@
 class CartItemsController < ApplicationController
-  before_action :require_login, only:[:index , :new, :edit]
+  before_action :require_login
+  before_action :get_product, except:[:index, :destroy]
 
   def index
-      @cart = current_user.cart
-      if current_user.cart.present?
-        @cart_items = current_user.cart.cart_items
-      end
+    @cart = current_user.cart
+    if @cart
+      @products = current_user.cart.products
+      @cart_items = @cart.cart_items
+    end
   end
   
   def create
-    if current_user
-      if current_user.cart.present?
-        @product = Product.find_by_id(params[:id])
-        @cart_items = current_user.cart.cart_items
-        product_price = @product.price
-        item = @cart_items.find_by(product_id: @product.id)
-        # GST 2% on each product
-        if item
-          item = item.increment(:quantity, 1)
-          quantity = item.quantity
-          item.save
-          # quantity += 1
-        else
-          quantity = 1
-        end
-        cgst = product_price * 0.02.to_d  * quantity
-        sgst = product_price *0.02 * quantity
-        total_tax = cgst + sgst 
-        final_price = product_price * quantity + total_tax
-        # Check product already present or not
-        if current_user.cart.cart_items.where(product_id: @product[:id]).present?
-          # update cart_items details
-          @cart_item =  @product.cart_items.update(cgst: cgst, sgst: sgst, total_tax: total_tax, price: final_price)
-          # find total price of cart_item
-          total_price = @cart_items.sum(:price)
-          cart = current_user.cart.update(total_price: total_price)
-        else
-          current_user.cart.update(total_price: final_price) 
-          @cart_item = @product.cart_items.create(quantity: quantity, cgst: cgst, sgst: sgst, total_tax: total_tax, price: final_price, cart_id: current_user.cart.id)
-        end
-      else
-        @cart = current_user.create_cart(total_price: product_price)
-        @product = Product.find_by_id(params[:id])
-        quantity = 1
-        cgst = product_price * 0.02.to_d  * quantity
-        sgst = product_price *0.02 * quantity
-        total_tax = cgst + sgst 
-        final_price = product_price * quantity + total_tax
-        current_user.cart.update(total_price: final_price) 
-        @cart_item = @product.cart_items.create(quantity: quantity, cgst: cgst, sgst: sgst, total_tax: total_tax, price: final_price, cart_id: current_user.cart.id)
-      end 
+    if current_user.cart.present?
+      @cart = current_user.cart
     else
-      flash[:danger] = "You must be logged in to access this section" 
-      redirect_to sessions_new_path
+      @cart = current_user.create_cart(total_price: product_price)
     end
+    @cart_items = @cart.cart_items
+    product_price = @product.price
+    # find_or_initialze_by
+    item = @cart_items.find_by(product_id: product.id)
+    # GST 2% on each product
+    if item
+      item = item.increment(:quantity, 1)
+      quantity = item.quantity
+      item.save
+    else
+      quantity = 1
+    end
+    cgst, sgst, total_tax, final_price = calculate_tax(product_price, quantity)
+    # Check product already present or not
+    if @cart.cart_items.where(product_id: @product[:id]).present?
+      # update cart_items details
+      @cart_item =  product.cart_items.update(cgst: cgst, sgst: sgst, total_tax: total_tax, price: final_price)
+      # find total price of cart_item
+      total_price = @cart_items.sum(:price)
+      cart = @cart.update(total_price: total_price)
+    else
+      @cart.update(total_price: final_price) 
+      @cart_item = @product.cart_items.create(quantity: quantity, cgst: cgst, sgst: sgst, total_tax: total_tax, price: final_price, cart_id: current_user.cart.id)
+    end 
   end
 
   def destroy
     @cart_items = current_user.cart.cart_items
-    @cart_item = @cart_items.find_by_id(params[:id])
+    @cart_item = @cart_items.find(id: params[:id])
+    byebug
     @cart_item.destroy
     total_price = @cart_items.sum(:price)
     cart = current_user.cart.update(total_price: total_price)
@@ -69,46 +56,45 @@ class CartItemsController < ApplicationController
 
   def decrease_quantity
     @cart_items = current_user.cart.cart_items
-    cart_item = current_user.cart.cart_items.find_by(product_id: params[:id])
-    product = Product.find(params[:id])
-    product_price = product.price
-    cart_item.decrement(:quantity, 1)
-    quantity = cart_item.quantity
-    # product_price = product.price
-    cgst = product_price * 0.02.to_d  * quantity
-    sgst = product_price *0.02 * quantity
-    total_tax = cgst + sgst 
-    final_price = product_price * quantity + total_tax
-    total_price = @cart_items.sum(:price)
-    cart = current_user.cart.update(total_price: total_price)
-    cart_item = cart_item.update(quantity: quantity, cgst: cgst, sgst: sgst, total_tax: total_tax, price: final_price)
-    render :index
+    item = @cart_items.find_by(product_id: params[:id])
+    product_price = @product.price
+    item.decrement(:quantity, 1)
+    quantity = item.quantity
+    cgst, sgst, total_tax, final_price = calculate_tax(product_price, quantity)
+    update_cart_and_cart_item(quantity, cgst, sgst, total_tax, final_price,item)
   end
 
    def increase_quantity
     @cart_items = current_user.cart.cart_items
-    cart_item = current_user.cart.cart_items.find_by(product_id: params[:id])
-    product = Product.find(params[:id])
-    product_price = product.price
-    cart_item.increment(:quantity, 1)
-    quantity = cart_item.quantity
+    item = @cart_items.find_by(product_id: params[:id])
+    # product = get_product
+    product_price = @product.price
+    item.increment(:quantity, 1)
+    quantity = item.quantity
     # product_price = product.price
-    cgst = product_price * 0.02.to_d  * quantity
-    sgst = product_price *0.02 * quantity
-    total_tax = cgst + sgst 
-    final_price = product_price * quantity + total_tax
-    total_price = @cart_items.sum(:price)
-    cart = current_user.cart.update(total_price: total_price)
-    cart_item = cart_item.update(quantity: quantity, cgst: cgst, sgst: sgst, total_tax: total_tax, price: final_price)
-    render :index
+    cgst, sgst, total_tax, final_price = calculate_tax(product_price, quantity)
+    update_cart_and_cart_item(quantity, cgst, sgst, total_tax, final_price, item)
   end
 
   private
 
-  def require_login
-    unless current_user  
-      flash[:danger] = "You must be logged in to access this section"
-      redirect_to sessions_new_path
-    end
+  def get_product
+    @product = Product.find(params[:id])
   end
+
+  def  calculate_tax(product_price, quantity)
+    cgst = product_price * 0.02.to_d  * quantity
+    sgst = product_price *0.02 * quantity
+    total_tax = cgst + sgst 
+    final_price = product_price * quantity + total_tax
+    return cgst, sgst, total_tax, final_price
+  end
+
+  def update_cart_and_cart_item(quantity, cgst, sgst, total_tax, final_price, item)
+    total_price = @cart_items.sum(:price)
+    cart = current_user.cart.update(total_price: total_price)
+    item = item.update(quantity: quantity, cgst: cgst, sgst: sgst, total_tax: total_tax, price: final_price)
+    redirect_to cart_items_path
+  end
+
 end
