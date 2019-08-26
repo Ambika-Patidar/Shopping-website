77b140628332b64c54103f82d8400f849e2f3674
ambikapatidar <apatidar@gammastack.com>
2019-08-26 20:11:26 +0530

    modified home contoller and home views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,8 @@
 class HomeController < ApplicationController
   layout "login"
+  
   def index
     @products = Product.all
-  end
-
-  def show
-    @product = Product.find_by_id(params[:id])
   end
 
 end
