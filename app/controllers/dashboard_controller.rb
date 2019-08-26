c61da531d86240471660caeb6b22b008f536da73
ambikapatidar <apatidar@gammastack.com>
2019-08-26 20:33:27 +0530

    updated dashboard controller and views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,22 +1,8 @@
 class DashboardController < ApplicationController
-  before_action :require_login, only:[ :index]
-  # before_action :require_sign_up, only:[ :index]
+  before_action :require_login, only:[:index]
+  
   def index
-    @products = Product.all
-    # Welcome to current_user.first_name
-  end
-
-  def show
-    @product = Product.find_by_id(params[:id])
-  end
-
-  private
-
-  def require_login
-    unless current_user  
-     flash[:danger] = "You must be logged in to access this section"
-     redirect_to sessions_new_path
-    end
+    @products = Product.where.not(user_id: current_user.id)
   end
 
 end
