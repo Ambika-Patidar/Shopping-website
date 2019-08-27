7cdde90aea87ce5b96b0f9e454afdbb7fa8d0c6d
ambikapatidar <apatidar@gammastack.com>
2019-08-27 18:40:32 +0530

    Updated Address Contoller and View

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -12,9 +12,20 @@ class AddressesController < ApplicationController
   def create
     @address = current_user.addresses.build(address_params)
     if @address.save
+      flash[:info] = "Successfully Added delivery address"
       redirect_to order_items_path
     else 
       render 'new'
+    end
+  end
+
+  def destroy
+    @addresses = current_user.addresses
+    address = @addresses.find_by(id: params[:id])
+    byebug
+    if address.destroy
+      flash[:info] = "Successfully Address  Deleted "
+      redirect_to addresses_path
     end
   end
 
--- app/models/address.rb
+++ app/models/address.rb
@@ -3,6 +3,5 @@ class Address < ApplicationRecord
   has_many :orders
   
   validates_presence_of :building_no, :area, :city, :state, :pincode
-  validates_format_of :city, :state, :with => /\A[a-zA-Z]+\z/
   validates_format_of :pincode, :with => /\A[0-9,\s]+\z/
 end
