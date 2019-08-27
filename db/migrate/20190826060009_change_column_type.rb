15fbfdb003d95b6ce671e95aa12bc3ea26118820
ambikapatidar <apatidar@gammastack.com>
2019-08-27 16:21:19 +0530

    Updated CSS file and db migrate files

--- db/migrate/20190826060009_change_column_type.rb
+++ db/migrate/20190826060009_change_column_type.rb
@@ -1,6 +1,7 @@
+ # frozen_string_literal: true.
 class ChangeColumnType < ActiveRecord::Migration[5.2]
   def change
-    change_column :addresses, :city , :string
+    change_column :addresses, :city, :string
     change_column :addresses, :state, :string
   end
 end
